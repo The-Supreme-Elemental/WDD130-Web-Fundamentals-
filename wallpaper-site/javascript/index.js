133ff2b1ac8b14954dc6f40969ccab131e914188
Christopher Hylton <jamboyh5@gmail.com>
2024-04-08 07:48:54 -0600

    additional slideshow and move flower summary

--- wallpaper-site/javascript/index.js
+++ wallpaper-site/javascript/index.js
@@ -1,20 +1,27 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex, 'slideshow1');
+showSlides(slideIndex, 'slideshow2');
+// showSlides(slideIndex, 'slideshow3');
+// showSlides(slideIndex, 'slideshow4');
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, button) {
+    let parentId = button.parentElement.id;
+  showSlides(slideIndex += n, parentId);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n, button) {
+    let parentId = button.parentElement.id;
+  showSlides(slideIndex = n, parentId);
 }
 
-function showSlides(n) {
+function showSlides(n, id) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+  let container = document.getElementById(id);
+  let slides = container.querySelectorAll(".mySlides");
+  let dots = document.querySelectorAll(".dot");
+
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
